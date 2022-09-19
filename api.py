f30d07f9c2a250527894d502251c565751ffac8a
amirSalmani <salmaniamir13@gmail.com>
2022-09-19 16:12:40 +0430

    created

---
+++ api.py
@@ -0,0 +1,37 @@
+import uvicorn
+from fastapi import FastAPI
+from data_json import data
+
+
+app = FastAPI()
+
+
+@app.get("/cities/")
+async def cities():
+    cities = []
+    for i in data.keys():
+        cities.append(i)
+
+    return {"cities": cities}
+
+
+@app.get("/region/{city_name}")
+async def cities(city_name):
+    region = []
+    for i in data.get(city_name):
+        region.append(i)
+
+    return {"regions": region}
+
+
+@app.get("/quarter/{city_name}/{region_name}/")
+async def cities(city_name, region_name):
+    quarter = []
+    for i in data.get(city_name).get(region_name):
+        quarter.append(i)
+
+    return {"quarter": quarter}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
